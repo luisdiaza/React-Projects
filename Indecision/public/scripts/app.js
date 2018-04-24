8bec7da862b6f134075c568d81c82c9ba9ab6a4d
Luis Diaz <diazluis1048@gmail.com>
2018-04-24 14:47:11 -0400

    explored basics of conditional rendering in JSX

--- Indecision/public/scripts/app.js
+++ Indecision/public/scripts/app.js
@@ -2,12 +2,66 @@
 
 console.log('App.js is running!');
 
-//JSX - JavaScript XML
-var template = React.createElement(
-  'p',
-  null,
-  'Does this change?!'
+var object = {
+    title: 'Dragonball',
+    subtitle: 'Super',
+    options: ['One', 'Two']
+
+    //JSX - JavaScript XML
+};var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        object.title
+    ),
+    object.subtitle && React.createElement(
+        'p',
+        null,
+        object.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        object.options.length > 0 ? 'Here are your options' : 'No options'
+    )
 );
+
+var user = {
+    name: 'Mike',
+    age: '17',
+    location: 'North Plainfield'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
 var app = document.getElementById('app');
 
 ReactDOM.render(template, app);
--- Indecision/src/app.js
+++ Indecision/src/app.js
@@ -1,7 +1,42 @@
 console.log('App.js is running!');
 
+const object = {
+    title: 'Dragonball',
+    subtitle: 'Super',
+    options: ['One', 'Two']
+}
+
+
 //JSX - JavaScript XML
-const template = <p>Does this change?!</p>;
+const template = (
+    <div>
+        <h1>{object.title}</h1>
+        {object.subtitle && <p>{object.subtitle}</p>}
+        <p>{object.options.length > 0 ? 'Here are your options' : 'No options' }</p>
+
+    </div>
+);
+
+const user = {
+    name: 'Mike',
+    age: '17',
+    location: 'North Plainfield'
+}
+
+function getLocation(location) {
+    if(location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
+const templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+)
+
 const app =  document.getElementById('app');
 
 ReactDOM.render(template,app);
