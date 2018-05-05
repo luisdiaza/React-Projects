824bb269337fcca4de0e1ac9793695e1a57f4c41
Luis Diaz <diazluis1048@gmail.com>
2018-05-05 13:09:40 -0400

    finished building out forms and inputs for indecision app and updated readme

--- Indecision/public/scripts/app.js
+++ Indecision/public/scripts/app.js
@@ -5,53 +5,91 @@ console.log('App.js is running!');
 var object = {
     title: 'Dragonball',
     subtitle: 'Super',
-    options: ['One', 'Two']
+    options: []
+};
 
-    //JSX - JavaScript XML
-};var template = //These are objects
-React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        object.title
-    ),
-    object.subtitle && React.createElement(
-        'p',
-        null,
-        object.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        object.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    ),
-    React.createElement(
-        'form',
-        { onSubmit: onForm },
-        React.createElement('input', { type: 'text', name: 'option' }),
-        React.createElement(
-            'button',
-            null,
-            'Add Option'
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+
+    /*
+        .target: points to the the element that the event started on. In this case, it is forms.
+        .elements: contains a list of all elements indexed by name
+    */
+
+    if (option) {
+        //Remember, an if statement executes if the condition is truthy. So if there is content inside option, the if statement will run
+        object.options.push(option);
+        e.target.elements.option.value = '';
+        renderFunction();
+    }
+};
+
+var onRemoveAll = function onRemoveAll() {
+    object.options = [];
+    renderFunction();
+};
 
 var app = document.getElementById('app');
-ReactDOM.render(template, app);
+
+var renderFunction = function renderFunction() {
+    //JSX - JavaScript XML
+    var template = //These are objects
+    React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            object.title
+        ),
+        object.subtitle && React.createElement(
+            'p',
+            null,
+            object.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            object.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            object.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+    ReactDOM.render(template, app);
+};
+
+renderFunction(); //renders the intial application
--- Indecision/src/app.js
+++ Indecision/src/app.js
@@ -3,27 +3,54 @@ console.log('App.js is running!');
 const object = {
     title: 'Dragonball',
     subtitle: 'Super',
-    options: ['One', 'Two']
+    options: []
 }
 
+const onFormSubmit = (e) => {
+    e.preventDefault();
 
-//JSX - JavaScript XML
-const template = ( //These are objects
-    <div>
-        <h1>{object.title}</h1>
-        {object.subtitle && <p>{object.subtitle}</p>}
-        <p>{object.options.length > 0 ? 'Here are your options' : 'No options' }</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-        <form onSubmit={onForm}>
-            <input type="text" name="option" ></input>
-            <button>Add Option</button>
-        </form>
+    const option = e.target.elements.option.value;
 
-    </div>
-);
+    /*
+        .target: points to the the element that the event started on. In this case, it is forms.
+        .elements: contains a list of all elements indexed by name
+    */
+
+    if (option) { //Remember, an if statement executes if the condition is truthy. So if there is content inside option, the if statement will run
+        object.options.push(option);
+        e.target.elements.option.value = '';
+        renderFunction();
+    }
+};
+
+const onRemoveAll = () => {
+    object.options = [];
+    renderFunction();
+};
 
 const app =  document.getElementById('app');
-ReactDOM.render(template,app);
+
+const renderFunction = () => {
+    //JSX - JavaScript XML
+    const template = ( //These are objects
+        <div>
+            <h1>{object.title}</h1>
+            {object.subtitle && <p>{object.subtitle}</p>}
+            <p>{object.options.length > 0 ? 'Here are your options' : 'No options' }</p>
+            <p>{object.options.length}</p>
+            <button onClick = {onRemoveAll}>Remove All</button>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" ></input>
+                <button>Add Option</button>
+            </form>
+
+        </div>
+    );
+    ReactDOM.render(template,app);
+};
+
+renderFunction();//renders the intial application
